fc25dec47f9b06edbeea343cd4515a10143c9151
reneener <jhjin2u@gmail.com>
2022-11-22 20:44:46 +0900

    init structure

---
+++ src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <input type="text" v-model="newUrl">
+  <button>추가</button>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      newUrl: ''
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
